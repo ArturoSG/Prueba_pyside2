cb7312a06105c3f98e3d561b32454753c86f3e20
ArturoSG <70913541+ArturoSG@users.noreply.github.com>
2021-04-11 19:41:42 -0500

    Add files via upload

---
+++ prueba_pyside2/mainwindow.py
@@ -0,0 +1,20 @@
+from PySide2.QtWidgets import QMainWindow
+from PySide2.QtCore import Slot
+from ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
+
+    @Slot()
+    def click_agregar(self):
+        titulo = self.ui.titulo_lineEdit.text()
+        autor = self.ui.autor_lineEdit.text()
+        publicado = self.ui.publicado_spinBox.value()
+        editorial = self.ui.editorial_lineEdit.text()
+
+        print(titulo, autor, publicado, editorial)
+        self.ui.salida.insertPlainText(titulo + autor + str(publicado) + editorial)
